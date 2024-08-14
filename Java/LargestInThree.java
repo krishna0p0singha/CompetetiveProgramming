9c8459823883da81f2035553891f31cd832613f5
cyber0bravo <cyber0bravo@gmail.com>
2024-08-14 06:49:22 +0530

    JAVA

---
+++ Java/LargestInThree.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class  LargestInThree{
+	public static void main(String [] args){ 
+		Scanner sc=new Scanner(System.in); 
+		System.out.print("Enter first number => ");
+		int a=sc.nextInt();
+		System.out.print("Enter Second number => ");
+		int b=sc.nextInt();
+		System.out.print("Enter Third number => ");
+                int c=sc.nextInt();
+		int result = a>b && a>c ? a : b>c && b>a ? b : c;
+		System.out.println(result + " is Largest Number");
+	}
+}
